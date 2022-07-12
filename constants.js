b87a48a671dc12f672d644080bc09ca8fa4846a7
Jason Hoyt <hoytjmatthews@gmail.com>
2022-07-12 17:36:43 -0400

    first commit

---
+++ constants.js
@@ -0,0 +1,16 @@
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{`${card.number}`}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+    <SectionDivider />
+  </Section>
+);
+
+export default Acomplishments;
